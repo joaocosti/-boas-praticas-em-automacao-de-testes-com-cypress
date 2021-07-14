cffdc0448e194604835bb03b8d93cb295a239aaa
joao <joaomartins@vipcommerce.com.br>
2021-07-13 21:27:40 -0300

    Retirando codigo desnecessario e verificando atributos de redirecionamento dos botoes

--- cypress/integration/browser-testing/sample1.spec.js
+++ cypress/integration/browser-testing/sample1.spec.js
@@ -1,11 +1,9 @@
 describe('Browser testing bad practice - Sample 1', () => {
-  beforeEach(() => {
-    cy.visit('https://notes-serverless-app.com')
-  })
+	beforeEach(() => {
+		cy.visit('https://notes-serverless-app.com');
+	});
 
-  it('directs the user to the login page when clicking the login link', () => {
-    cy.contains('.nav a', 'Login').click()
-
-    cy.url().should('be.equal', 'https://notes-serverless-app.com/login')
-  })
-})
+	it('directs the user to the login page when clicking the login link', () => {
+		cy.contains('.nav a', 'Login').should('have.attr', 'href', '/login').and('not.have.attr', 'target');
+	});
+});
